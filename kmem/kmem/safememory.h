86ce911f10f11b520fa2cd9613be85590fc41804
Samuel Tulach <samtulach@gmail.com>
2020-07-21 13:06:15 +0200

    another seh

--- kmem/kmem/safememory.h
+++ kmem/kmem/safememory.h
@@ -5,7 +5,7 @@
 
 static unsigned long long TotalFails = 0;
 
-BOOLEAN CopyMemory(PVOID destination, PVOID source, SIZE_T size, BOOLEAN intersects)
+__forceinline BOOLEAN CopyMemory(PVOID destination, PVOID source, SIZE_T size, BOOLEAN intersects)
 {
     switch (size)
 	{
@@ -56,49 +56,62 @@ BOOLEAN CopyMemory(PVOID destination, PVOID source, SIZE_T size, BOOLEAN interse
 
 void CopyProcessMemory(PEPROCESS sourceProcess, PVOID sourceAddress, PEPROCESS targetProcess, PVOID targetAddress, SIZE_T size)
 {
+    ULONG tag = 'ihhG';
+    PVOID kernelBuffer = ExAllocatePoolWithTag(NonPagedPool, size, tag);
+
+    KAPC_STATE apcState;
+    KeStackAttachProcess(sourceProcess, &apcState);
+
+    HANDLE secureMemoryRead = MmSecureVirtualMemory(sourceAddress, size, PAGE_READONLY);
+    if (!secureMemoryRead)
+    {
+        KeUnstackDetachProcess(&apcState);
+        ExFreePoolWithTag(kernelBuffer, tag);
+        return;
+    }
+
+    // Weird except handlers that seem unnecessarry, because the same code that is in except is under it
+    // but it's windows and it's exception handling
     __try 
     {
-        ULONG tag = 'ihhG';
-        PVOID kernelBuffer = ExAllocatePoolWithTag(NonPagedPool, size, tag);
-
-        KAPC_STATE apcState;
-        KeStackAttachProcess(sourceProcess, &apcState);
-
-        HANDLE secureMemoryRead = MmSecureVirtualMemory(sourceAddress, size, PAGE_READONLY);
-        if (!secureMemoryRead)
-        {
-            KeUnstackDetachProcess(&apcState);
-            ExFreePoolWithTag(kernelBuffer, tag);
-            return;
-        }
-
         ProbeForRead(sourceAddress, size, 1);
         CopyMemory(kernelBuffer, sourceAddress, size, FALSE);
-
+    }
+    __except (1) 
+    {
         MmUnsecureVirtualMemory(secureMemoryRead);
         KeUnstackDetachProcess(&apcState);
+        ExFreePoolWithTag(kernelBuffer, tag);
+        return;
+    }
 
-        KeStackAttachProcess(targetProcess, &apcState);
-        HANDLE secureMemoryWrite = MmSecureVirtualMemory(targetAddress, size, PAGE_READWRITE);
-        if (!secureMemoryWrite)
-        {
-            KeUnstackDetachProcess(&apcState);
-            ExFreePoolWithTag(kernelBuffer, tag);
-            return;
-        }
+    MmUnsecureVirtualMemory(secureMemoryRead);
+    KeUnstackDetachProcess(&apcState);
 
+    KeStackAttachProcess(targetProcess, &apcState);
+    HANDLE secureMemoryWrite = MmSecureVirtualMemory(targetAddress, size, PAGE_READWRITE);
+    if (!secureMemoryWrite)
+    {
+        KeUnstackDetachProcess(&apcState);
+        ExFreePoolWithTag(kernelBuffer, tag);
+        return;
+    }
+
+    __try
+    {
         ProbeForWrite(targetAddress, size, 1);
         CopyMemory(targetAddress, kernelBuffer, size, FALSE);
-
-        MmUnsecureVirtualMemory(secureMemoryWrite);
-        KeUnstackDetachProcess(&apcState);
-
-        ExFreePoolWithTag(kernelBuffer, tag);
     }
     __except (1)
     {
-        // SEH exception handler
-        // In ideal scenario should not happen
-        TotalFails++; // Windows is high quality and not having anything here = not good
+        MmUnsecureVirtualMemory(secureMemoryWrite);
+        KeUnstackDetachProcess(&apcState);
+        ExFreePoolWithTag(kernelBuffer, tag);
+        return;
     }
+
+    MmUnsecureVirtualMemory(secureMemoryWrite);
+    KeUnstackDetachProcess(&apcState);
+
+    ExFreePoolWithTag(kernelBuffer, tag);
 }
